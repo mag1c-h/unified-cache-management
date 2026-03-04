a66b99e5ba96bf1d99c26ef169044efb92d1c886
Mag1c.H <hemajun815@163.com>
2026-03-04 10:15:35 +0800

    AsyncStore参数解析

---
+++ ucm/store/async/cc/async_store.cc
@@ -0,0 +1,66 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+#include "global_config.h"
+#include "ucmstore_v1.h"
+
+namespace UC::AsyncStore {
+
+class AsyncStore : public StoreV1 {
+public:
+    Status Setup(const Detail::Dictionary& param) override
+    {
+        Config config;
+        auto status = config.Parse(param);
+        if (status.Failure()) {
+            UC_ERROR("Failed to parse config: {}.", status);
+            return status;
+        }
+        config.Show();
+        return Status::OK();
+    }
+    std::string Readme() const override { return "AsyncStore"; }
+    Expected<std::vector<uint8_t>> Lookup(const Detail::BlockId* blocks, size_t num) override
+    {
+        return Status::Unsupported();
+    }
+    Expected<ssize_t> LookupOnPrefix(const Detail::BlockId* blocks, size_t num) override
+    {
+        return Status::Unsupported();
+    }
+    void Prefetch(const Detail::BlockId* blocks, size_t num) override {}
+    Expected<Detail::TaskHandle> Load(Detail::TaskDesc task) override
+    {
+        return Status::Unsupported();
+    }
+    Expected<Detail::TaskHandle> Dump(Detail::TaskDesc task) override
+    {
+        return Status::Unsupported();
+    }
+    Expected<bool> Check(Detail::TaskHandle taskId) override { return Status::Unsupported(); }
+    Status Wait(Detail::TaskHandle taskId) override { return Status::Unsupported(); }
+};
+
+}  // namespace UC::AsyncStore
+
+extern "C" UC::StoreV1* MakeAsyncStore() { return new UC::AsyncStore::AsyncStore(); }
---
+++ ucm/store/async/cc/global_config.h
@@ -0,0 +1,97 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+#ifndef UNIFIEDCACHE_ASYNC_STORE_CC_GLOBAL_CONFIG_H
+#define UNIFIEDCACHE_ASYNC_STORE_CC_GLOBAL_CONFIG_H
+
+#include <string>
+#include <vector>
+#include "logger/logger.h"
+#include "status/status.h"
+#include "type/dictionary.h"
+
+namespace UC::AsyncStore {
+
+class Config {
+public:
+    std::vector<std::string> storageBackends{};
+    int32_t deviceId{-1};
+    size_t tensorSize{0};
+    size_t shardSize{0};
+    size_t blockSize{0};
+    bool ioDirect{false};
+    size_t dataTransConcurrency{8};
+    size_t lookupConcurrency{8};
+    size_t timeoutMs{30000};
+    size_t dataDirShardBytes{3};
+
+    Status Parse(const Detail::Dictionary& param)
+    {
+        param.Get("storage_backends", storageBackends);
+        param.GetNumber("device_id", deviceId);
+        param.GetNumber("tensor_size", tensorSize);
+        param.GetNumber("shard_size", shardSize);
+        param.GetNumber("block_size", blockSize);
+        param.Get("io_direct", ioDirect);
+        param.GetNumber("posix_data_trans_concurrency", dataTransConcurrency);
+        param.GetNumber("posix_lookup_concurrency", lookupConcurrency);
+        param.GetNumber("timeout_ms", timeoutMs);
+        param.GetNumber("data_dir_shard_bytes", dataDirShardBytes);
+        if (storageBackends.empty()) { return Status::InvalidParam("invalid storage backends"); }
+        if (deviceId < -1) { return Status::InvalidParam("invalid device({})", deviceId); }
+        if (dataTransConcurrency == 0 || lookupConcurrency == 0) {
+            return Status::InvalidParam("invalid concurrency({},{})", dataTransConcurrency,
+                                        lookupConcurrency);
+        }
+        if (dataDirShardBytes > 5) {
+            return Status::InvalidParam("invalid shard bytes({})", dataDirShardBytes);
+        }
+        if (deviceId == -1) { return Status::OK(); }
+        if (tensorSize == 0 || shardSize < tensorSize || blockSize < shardSize ||
+            shardSize % tensorSize != 0 || blockSize % shardSize != 0) {
+            return Status::InvalidParam("invalid size({},{},{})", tensorSize, shardSize, blockSize);
+        }
+        return Status::OK();
+    }
+    void Show()
+    {
+        constexpr const char* ns = "AsyncStore";
+        std::string buildType = UCM_BUILD_TYPE;
+        if (buildType.empty()) { buildType = "Release"; }
+        UC_INFO("{}-{}({}).", ns, UCM_COMMIT_ID, buildType);
+        UC_INFO("Set {}::StorageBackends to {}.", ns, storageBackends);
+        UC_INFO("Set {}::DeviceId to {}.", ns, deviceId);
+        UC_INFO("Set {}::TensorSize to {}.", ns, tensorSize);
+        UC_INFO("Set {}::ShardSize to {}.", ns, shardSize);
+        UC_INFO("Set {}::BlockSize to {}.", ns, blockSize);
+        UC_INFO("Set {}::IoDirect to {}.", ns, ioDirect);
+        UC_INFO("Set {}::DataTransConcurrency to {}.", ns, dataTransConcurrency);
+        UC_INFO("Set {}::LookupConcurrency to {}.", ns, lookupConcurrency);
+        UC_INFO("Set {}::TimeoutMs to {}.", ns, timeoutMs);
+        UC_INFO("Set {}::DataDirShardBytes to {}.", ns, dataDirShardBytes);
+    }
+};
+
+}  // namespace UC::AsyncStore
+
+#endif
